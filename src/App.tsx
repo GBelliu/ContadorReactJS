a3bfa7816d6eb9bc8ee6326c542fc6683d98c237
Ruba <nathan.ruba2003@gmail.com>
2024-02-21 22:05:56 -0300

    fod

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App = () => {
     <div>
       <button onClick={handleMinus}>-</button>
       <div>{n}</div>
+      <div>fod</div>
       <button onClick={handleMore}>+</button>
     </div>
   );
